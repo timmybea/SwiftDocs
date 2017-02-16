fb2eb315ca12a3dad13925c1ce25f12f90cb3e45
Tim Beals <tim.beals@gmail.com>
2017-02-16 10:56:29 -0800

    input parameter variations

--- 6.Functions/Functions.playground/Contents.swift
+++ 6.Functions/Functions.playground/Contents.swift
@@ -64,5 +64,51 @@ let answer = findMinMaxIn(array: [4, 8, 12, 3, 9, 1])
 answer.min
 answer.max
 
+//Note that it is possible for the tuple to be optional (Int, Int)? or for the values in the tuple to be optional (Int?, Int?)
+
+
+func findOptionalMinMaxIn(array: [Int]) -> (min: Int, max: Int)? {
+    if array.isEmpty { return nil }
+    var min: Int = array[0]
+    var max: Int = array[0]
+    
+    for value in array {
+        if value < min {
+            min = value
+        } else if value > max {
+            max = value
+        }
+    }
+    return (min, max)
+}
+
+//If the return is an optional, you will want to use optional binding to check if there is a value in the return.
+
+if let bounds = findOptionalMinMaxIn(array: [4, 6, 1, 9, 7]) {
+    print("The minimum bound is \(bounds.min) and the maximum is \(bounds.max)")
+} else {
+    print("The array was empty")
+}
+
+
+
+//naming input parameters:
+//Notice that in this function, we can use the decriptive variable name 'hometown' in the function, but when we call the function we can just use 'from'. This makes Swift incredibly readable.
+
+func greet(person: String, from hometown: String) -> String {
+    return "Hello \(person)!  Glad you could visit from \(hometown)."
+}
+print(greet(person: "Bill", from: "Cupertino"))
+// Prints "Hello Bill!  Glad you could visit from Cupertino."
+
+
+//Ommitting input parameter names:
+//Sometimes this could improve the readability when the function name itself decribes the input parameter. Notice that I can still use 'age' inside the function, but the underscore means that I don't have to have 'age' in the function call.
+
+func stateMyAge(_ age: Int) -> String {
+    return "I am \(age) years old"
+}
+
+stateMyAge(34)
 
 
